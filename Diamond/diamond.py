02a92f04c100c06705268b662bd863ac17f6e756
gmeline <152296568+gmeline@users.noreply.github.com>
2023-12-21 11:02:42 +0100

    Update diamond.py

--- Diamond/diamond.py
+++ Diamond/diamond.py
@@ -3,22 +3,25 @@ alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 def diamond(lettre):
     nb=0
     previus = 1
+    buffer = ''
     while(alphabet[nb]!=lettre):
         nb += 1
     for i in range(nb+1):
         if(i==0):
-            print(nb*' '+alphabet[i])
+            buffer+= nb*' '+alphabet[i]+'\n'
         else:
-            print((nb-i)*' '+alphabet[i]+previus*' '+alphabet[i])
+            buffer+=(nb-i)*' '+alphabet[i]+previus*' '+alphabet[i]+'\n'
             previus +=2
-    
+
     previus-=4
     for i in range(nb-1,-1,-1):
         if(i==0):
-            print(nb*' '+alphabet[i])
+            buffer+=nb*' '+alphabet[i]+'\n'
         else:
-            print((nb-i)*' '+alphabet[i]+previus*' '+alphabet[i])
+            buffer+=(nb-i)*' '+alphabet[i]+previus*' '+alphabet[i]+'\n'
             previus -=2
+    print(buffer)
+    return (buffer)
 
 if __name__ == '__main__':
     diamond('C')
