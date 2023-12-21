1f23baf1a4090c5a868f0967a5c27a626308714a
gmeline <152296568+gmeline@users.noreply.github.com>
2023-12-21 10:39:32 +0100

    Update diamond.py

--- Diamond/diamond.py
+++ Diamond/diamond.py
@@ -1,10 +1,24 @@
-const alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 def diamond(lettre):
-  var i=0
-  while(alphabet[i]!=lettre):
-    print(alphabet[i]+'\n')
-    i++
-  while(i>=0):
-    print(alphabet[i]+'\n')
-    i--
+    nb=0
+    previus = 1
+    while(alphabet[nb]!=lettre):
+        nb += 1
+    for i in range(nb+1):
+        if(i==0):
+            print(nb*' '+alphabet[i])
+        else:
+            print((nb-i)*' '+alphabet[i]+previus*' '+alphabet[i])
+            previus +=2
+    
+    previus-=4
+    for i in range(nb-1,-1,-1):
+        if(i==0):
+            print(nb*' '+alphabet[i])
+        else:
+            print((nb-i)*' '+alphabet[i]+previus*' '+alphabet[i])
+            previus -=2
+
+if __name__ == '__main__':
+    diamond('C')
