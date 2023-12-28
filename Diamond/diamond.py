1a564fc1ac7101906a7f7b34bf15806c6a4efaf0
ThibautRingeval <78933047+ThibautRingeval@users.noreply.github.com>
2023-12-28 10:29:16 +0100

    Update avec pep 8 diamond.py

--- Diamond/diamond.py
+++ Diamond/diamond.py
@@ -1,28 +1,30 @@
 alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+
 def diamond(lettre):
     lettre=lettre.upper()
-    nb=0
+    nb = 0
     previus = 1
     buffer = ''
-    while(alphabet[nb]!=lettre):
+    while alphabet[nb] != lettre:
         nb += 1
     for i in range(nb+1):
-        if(i==0):
-            buffer+= nb*' '+alphabet[i]+'\n'
+        if i == 0:
+            buffer += nb*' '+alphabet[i]+'\n'
         else:
-            buffer+=(nb-i)*' '+alphabet[i]+previus*' '+alphabet[i]+'\n'
-            previus +=2
+            buffer += (nb-i)*' '+alphabet[i]+previus*' '+alphabet[i]+'\n'
+            previus += 2
 
-    previus-=4
-    for i in range(nb-1,-1,-1):
-        if(i==0):
-            buffer+=nb*' '+alphabet[i]+'\n'
+    previus -= 4
+    for i in range(nb - 1, - 1, -1):
+        if i == 0:
+            buffer += nb*' '+alphabet[i]+'\n'
         else:
-            buffer+=(nb-i)*' '+alphabet[i]+previus*' '+alphabet[i]+'\n'
-            previus -=2
+            buffer += (nb-i)*' '+alphabet[i]+previus*' '+alphabet[i]+'\n'
+            previus -= 2
     print(buffer)
-    return (buffer)
+    return buffer
+
 
 if __name__ == '__main__':
     diamond('C')
